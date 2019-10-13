c2d35f6d87a090d15df5a826beb72b3f71bc944d
Nick Spinale <nick@nickspinale.com>
2019-10-13 18:40:25 +0000

    move ocaml modules into nixpkgs

--- isabelle/components/nunchaku/default.nix
+++ isabelle/components/nunchaku/default.nix
@@ -1,25 +1,16 @@
-{ mkComponent, callPackage, ocamlPackages }:
+{ mkComponent, ocamlPackages }:
 
 let
-  nunchaku = callPackage ./nunchaku.nix rec {
-    menhir = ocamlPackages.menhir.override {
-      version = "20170712";
-    };
-    inherit sequence;
-  };
+  inherit (ocamlPackages) nunchaku;
 
-  ocamlPackages = callPackage ({ ocamlPackages }: ocamlPackages) {};
-  sequence = callPackage ../smbc/sequence.nix {
-    inherit (ocamlPackages) buildDunePackage qtest result;
-  };
 in
+
+assert nunchaku.version == "0.5";
+
 mkComponent {
   inherit (nunchaku) name;
   settings = ''
     NUNCHAKU_HOME=${nunchaku}/bin
     NUNCHAKU_VERSION=0.5
   '';
-  passthru = {
-    inherit nunchaku;
-  };
 }
--- isabelle/components/smbc/default.nix
+++ isabelle/components/smbc/default.nix
@@ -1,28 +1,16 @@
-{ mkComponent, callPackage }:
+{ mkComponent, ocamlPackages }:
 
 let
-  smbc = callPackage ./smbc.nix {
-    inherit msat tip-parser sequence;
-  };
-
-  dolmen_0_4 = callPackage ./dolmen.nix {};
-  msat = callPackage ./msat.nix {
-    inherit dolmen_0_4;
-  };
-  tip-parser = callPackage ./tip-parser.nix {};
-  ocamlPackages = callPackage ({ ocamlPackages }: ocamlPackages) {};
-  sequence = callPackage ./sequence.nix {
-    inherit (ocamlPackages) buildDunePackage qtest result;
-  };
+  inherit (ocamlPackages) smbc;
 
 in
+
+assert smbc.version == "0.4.1";
+
 mkComponent {
   inherit (smbc) name;
   settings = ''
     SMBC_HOME=${smbc}/bin
     SMBC_VERSION=${smbc.version}
   '';
-  passthru = {
-    inherit smbc msat dolmen_0_4 tip-parser;
-  };
 }
